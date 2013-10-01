0e5a3c47d05ed7274cae97272444e59c4469ede5
Mariano Vassallo <mvassallo@ba.gov.ar>
2013-10-01 13:23:25 -0300

    cambios al ploteo realtime

--- analisis/realtime.sh
+++ analisis/realtime.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pv -L 10k  -B 10 -q  | awk '{if (NF==3){print $1; fflush()}}' | ./procesar.py | feedgnuplot --stream  0 --lines --dataid --autolegend --xlen 10000 --points 2
+pv -L 10k  -B 10 -q  | mawk -W interactive '{if (NF==3){print $1; fflush()}}' | ./procesar.py | feedgnuplot --stream  0 --lines --dataid --autolegend --xlen 1000  
