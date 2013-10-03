4334bac498e03bb4e4e4809520fe34acf7d0cda9
Mariano Vassallo <mvassallo@ba.gov.ar>
2013-10-03 14:35:25 -0300

    saqué el rate limit de entrada al procesamiento realtime

--- analisis/realtime.sh
+++ analisis/realtime.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pv -L 10k  -B 10 -q  | mawk -W interactive '{if (NF==3){print $1; fflush()}}' | ./procesar.py | feedgnuplot --stream  0 --lines --dataid --autolegend --xlen 1000  
+mawk -W interactive '/[^a-zA-Z]/ {if (NF==3){print $1; fflush()}}' | ./procesar.py | feedgnuplot --stream  1 --lines --dataid --autolegend -xlen 5000  
