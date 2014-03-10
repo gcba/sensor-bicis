32644d9b215f6701770c2316b70f86fb2c9e5a6d
semanda <dicoycv@gmail.com>
2014-03-10 13:04:45 -0300

    logZMQ.py

---
+++ server/logZMQ.py
@@ -0,0 +1,30 @@
+import zmq
+from datetime import datetime
+#import ipdb
+import sqlite3
+
+DATABASE = 'base.db'
+db = sqlite3.connect(DATABASE)
+cur = db.cursor()
+if True:#try:
+#    cur.execute("CREATE TABLE bicis(id INT, dateTime INT, millis INT, pasadas INT)")
+#    db.commit()
+#except Exception:
+    maxID = 1 + cur.execute("select max(id) from bicis").fetchall()[0][0] 
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect("tcp://127.0.0.1:5000")
+#socket.connect("tcp://10.10.10.202:5000")
+socket.setsockopt(zmq.SUBSCRIBE, "ecobici1 bici")
+while True:
+    d = socket.recv().split()
+    print  ' '.join(d[2:])
+    ahora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    maxID += 1
+    cur.execute("insert into bicis values( %s , '%s' ,  '%s', %s)" % (maxID, ahora, ' '.join(d[2:]), 1))
+    db.commit()
+
+#cur.execute("CREATE TABLE log(id INT, id_sensor INT, dateTime INT, millis INT, valor INT);")
+#cur.execute("CREATE TABLE sensores(id_sensor INT, id_dispositivo INT, descripcion TEXT );")
+
